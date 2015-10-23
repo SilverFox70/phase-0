e19f8da58b97f63ece89864124fbbea8972343c6
SilverFox1970 <wfbrinkert@gmail.com>
2015-10-23 12:23:33 -0700

    Add files, solve 4.5.4

--- week-4/leap-years/my_solution.rb
+++ week-4/leap-years/my_solution.rb
@@ -1,6 +1,15 @@
 # Leap Years
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # Your Solution Below
+def leap_year?(year)
+	div_by_4 = true if year % 4 == 0
+	div_by_100 = true if year % 100 == 0
+	div_by_400 = true if year % 400 == 0
+	if (div_by_4 && !div_by_100) || (div_by_4 && div_by_100 && div_by_400)
+		return true
+	else return false
+	end
+end
