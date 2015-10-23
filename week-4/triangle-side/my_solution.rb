c660b22bfd7be10085c8693afa96678e5459309f
SilverFox1970 <wfbrinkert@gmail.com>
2015-10-23 12:46:25 -0700

    Add Pair solution for traingle

--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -1,8 +1,15 @@
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [with: Clinton Weber ].
 
 
 # Your Solution Below
 
 def valid_triangle?(a, b, c)
-  # Your code goes here!
+	if a == 0 || b == 0 || c == 0
+		return false
+end
+	if ((a + b) > c) && ((a + c ) > b) && ((b + c) > a)
+		return true
+	else
+		return false
+end
 end
