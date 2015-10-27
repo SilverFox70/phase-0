a70d6da06be48a92a368af10de3a9ca0bb28c9c3
SilverFox1970 <wfbrinkert@gmail.com>
2015-10-27 10:03:29 -0700

    Add Chris Gomes solution to group project

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -7,17 +7,43 @@ my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, 
 
 # Person 1's solution
 def my_array_finding_method(source, thing_to_find)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  result = [] # result is the output array
+  source.each do |word|
+    word_array = word.to_s.split(//) # This creates an array of charaters of 'word'
+    if word_array.include?(thing_to_find)
+      result.push(word)
+    end
+  end
+  return result
 end
 
 def my_hash_finding_method(source, thing_to_find)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  result = [] # result is the output array
+  source.each do |key, value|
+    if value == thing_to_find
+      result.push(key)
+    end
+  end
+  return result
 end
 
 # Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+=begin
+For the first method my_array_finding_method(source, thing_to_find),
+I used .each to iterate over the words in the array 'source'.
+For each string 'word' in 'source', I had to use .to_s to make 'word' into a string
+since the types of the words in source are not determined. Once 'word' is a string, then
+we can apply the string method .split(//), which takes the string and returns an array whose 
+elements are the letter of the string. Finally, I apply the array-method .include?('thing_to_find')
+to determine if 'thing_to_find' is an element of the array.
+After checking each word, we returned the array 'result'.
+For the second method my_hash_finding_method(source, thing_to_find),
+I used .each to iterate over the pairs {key, value} in the hash 'source'.
+For each value, I checked if 'value' equaled 'thing_to_find'.
+If true, then I pushed 'key' onto the array 'result.
+After checking each value, we returned the array 'result'.
+=end
+
 
 # Person 2
 def my_array_modification_method!(source, thing_to_modify)
