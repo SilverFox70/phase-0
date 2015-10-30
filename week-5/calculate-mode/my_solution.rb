a10351b8ab59871a57c4acf898ed5fed52862206
SilverFox1970 <wfbrinkert@gmail.com>
2015-10-30 11:20:32 -0700

    Our solution to 5.3

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -1,6 +1,6 @@
 # Calculate the mode Pairing Challenge
 
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge [with: Clinton Weber]
 
 # I spent [] hours on this challenge.
 
@@ -12,15 +12,41 @@
 
 # 0. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? An array of either numbers or strings
+# What is the output? An array of the most frequently occuring values in the source array
 # What are the steps needed to solve the problem?
+# 1. define a method to create a hash whose keys are the elements of the source array
+#    and whose values represent the frequency of occurences of said element
+# 2. if all values in our hash = 1, return the orginal array else
+# 3. if some values in our hash are greater than 1, return only those keys whose
+#    values are greater than 1, omitting the rest
 
 
 # 1. Initial Solution
+def frequencies(source)
+	freq = Hash.new
+	source.each do |e|
+		freq[e] = 0
+		source.each do |w|
+			freq[e] += 1 if e == w 
+		end
+	end
+	freq
+end
 
+def mode(source)
+	freq_hash = frequencies(source)
+	mode_array = []
+	all_only_once = true
+	if freq_hash.none? {|k, v| v > 1}
+		mode_array = source 
+	else freq_hash.each {|k, v| mode_array.push(k) if v > 1}
+	end
+	mode_array
+end
 
-
+p mode([1,1,2,3,4,5,5])
+p mode([1,2,3])
 
 # 3. Refactored Solution
 
