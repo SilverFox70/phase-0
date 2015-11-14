f30a0c25780f7676097f5242650fa18f77ea0ede
SilverFox1970 <wfbrinkert@gmail.com>
2015-11-14 10:31:06 -0800

    Solve 7.5, needs reflection

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -1,14 +1,34 @@
 // Separate Numbers with Commas in JavaScript **Pairing Challenge**
 
 
-// I worked on this challenge with: .
+// I worked on this challenge with: Bernice and Bill.
 
 // Pseudocode
-
+// input: a number as an integer
+// output: number separated with commas as string
+/*
+1. PLace holder variable ("result") to hold the result
+2. convert number to a string
+3. go backwards in a loop through string by 3's
+  a. insert a comma after 3rd number
+  b. store string of numbers and comma in "result"
+4. return the result
+*/
 
 
 // Initial Solution
-
+// function separateComma(number){
+//   var result = [];
+//   var numArray = number.toString().split("").reverse();
+//   for (var i = 0 ; i < numArray.length; i++){
+//     if ((i % 3 === 0)&&( i !== 0)){
+//       result.push(",");
+//     }
+//     result.push(numArray[i]);
+//     console.log(result);
+//   }
+//   return result.reverse().join('');
+// }
 
 
 
@@ -16,9 +36,9 @@
 
 
 
-
 // Your Own Tests (OPTIONAL)
-
+var numCom = separateComma(1569743456);
+console.log(numCom + " == "  + "1,569,743,456");
 
 
 
